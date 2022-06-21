3e6f6b960471c87469d1313b095113df5805936c
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2022-06-21 20:28:16 +0200

    Improved youtube player stuff

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -16,8 +16,7 @@ export class PostsService {
 
   wafrnMediaRegex = /\[wafrnmediaid="[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}"\]/gm;
   uuidRegex = /[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/;
-  youtubeLinkRegex = /(?:https?:\/\/)?(?:www\.|m\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[^&\s\?]+(?!\S))\/)|(?:\S*v=|v\/)))([^&\s\?]+)/gm;
-  youtubeRegex = /(?:https?:)?(?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube(?:\-nocookie)?\.(?:[A-Za-z]{2,4}|[A-Za-z]{2,3}\.[A-Za-z]{2})\/)(?:watch|embed\/|vi?\/)*(?:\?[\w=&]*vi?=)?([^#&\?\/]{11}).*?/;
+  youtubeRegex = /(?:https?:)?(?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube(?:\-nocookie)?\.(?:[A-Za-z]{2,4}|[A-Za-z]{2,3}\.[A-Za-z]{2})\/)(?:watch|embed\/|vi?\/)*(?:\?[\w=&]*vi?=)?([^#&\?\/]{11}).*?/g;
   public updateFollowers: BehaviorSubject<Boolean> = new BehaviorSubject(new Boolean());
 
   public followedUserIds: Array<String> = [];
@@ -114,21 +113,16 @@ export class PostsService {
 
     let sanitized = DOMPurify.sanitize(content, { ALLOWED_TAGS: ['b', 'i', 'u', 'a', 'span', 'br', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'strong', 'em', 'ul', 'li'] });
     // we remove stuff like img and script tags. we only allow certain stuff.
-    const youtubeLinks = sanitized.match(this.youtubeLinkRegex);
+    const youtubeLinks = sanitized.matchAll(this.youtubeRegex);
 
     if (youtubeLinks) {
-      youtubeLinks.forEach(youtubeString => {
+      Array.from(youtubeLinks).forEach(youtubeString => {
         // some exception, like when its on a href or stuff
-        if (youtubeString.indexOf('"') === -1) {
-          let ids = youtubeString.match(this.youtubeRegex);
-          if (ids) {
-            const videoId = ids[1];
-            const newString = '<app-wafrn-youtube-player video="' + videoId + '"></app-wafrn-youtube-player>';
-            sanitized = sanitized.replace(youtubeString, newString);
-          }
-        }
-      })
-    }
+            const newString = '<app-wafrn-youtube-player video="' + youtubeString[1] + '"></app-wafrn-youtube-player>';
+            sanitized = sanitized.replace(youtubeString[0], newString);
+        });
+      }
+    
 
 
 
