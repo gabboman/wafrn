a24c38607c8c3034acf6941440456f69ed9d78fd
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2022-09-21 19:44:50 +0200

    improved SEO integration with other platforms

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MetaDefinition } from '@angular/platform-browser';
 import { Title, Meta } from '@angular/platform-browser';
+import * as sanitizeHtml from 'sanitize-html';
 
 @Injectable({
   providedIn: 'root'
@@ -23,15 +24,24 @@ export class UtilsService {
   }
 
   setSEOTags(title: string, description: string, author: string, image: string): void{
-    
+    const sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: [],
+      allowedClasses: { },
+      allowedStyles: { }
+    });
     this.titleService.setTitle(title);
     this.metaTagService.addTags([
-      {name: 'description', content: description },
+      {name: 'description', content: sanitizedDescription },
       {name: 'author', content: author },
       {name: 'image', content: image},
-      {name: 'og:description', description },
+      {name: 'og:description', content: sanitizedDescription },
       {name: 'og:author', author },
-      {name: 'og:image', content: image}
+      {name: 'og:image', content: image},
+      {name: 'twitter:card', content: 'summary' },
+      {name: 'twitter:title', content: title },
+      {name: 'twitter:site', content: 'https://app.wafrn.net' },
+      {name: 'twitter:description', content: sanitizedDescription },
+      {name: 'twitter:image', content: image},
     ]);
     
     
