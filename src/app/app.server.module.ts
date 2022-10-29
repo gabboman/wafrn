ae53c18e402fef71980db3aca759f7ab3f7a8a54
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2022-10-29 21:00:17 +0200

    wupsie

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,13 +3,11 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { NgxIsrModule } from 'ngx-isr'; 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    NgxIsrModule,
     NoopAnimationsModule
   ],
   bootstrap: [AppComponent],
