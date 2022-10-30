bbefeb94a721b3587f6b94bbc2c7f80bdabd67d7
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2022-10-30 19:46:06 +0100

    bigger uploads

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   recaptchaPublic: '6LcrwQchAAAAADcgr0bLQ45AknhAQLkzX2RYNavT',
-  maxUploadSize: '50',
+  maxUploadSize: '250',
   baseUrl: 'https://api.wafrn.net',
   baseMediaUrl: 'https://media.wafrn.net',
   frontUrl: 'https://app.wafrn.net'
