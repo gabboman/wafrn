2f6b79c029b30f5c1a5cf015db57b2eb1a7dad02
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-04-14 15:52:31 +0200

    removed bunnycdn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   recaptchaPublic: '6LcrwQchAAAAADcgr0bLQ45AknhAQLkzX2RYNavT',
   maxUploadSize: '250',
   baseUrl: 'https://api.wafrn.net',
-  baseMediaUrl: 'https://wafrnmedia.b-cdn.net',
+  baseMediaUrl: 'https://media.wafrn.net',
   externalCacheurl: 'https://cache.wafrn.net/?media=',
   frontUrl: 'https://app.wafrn.net',
   shortenPosts: 5,
