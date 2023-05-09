bd82bceead9105fa5f8a9adf42b7a7a7546e9267
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-05-09 11:35:59 +0200

    initial ng16

--- src/app/resolvers/post.resolver.ts
+++ src/app/resolvers/post.resolver.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
+import { Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { from, Observable, of } from 'rxjs';
 import { ProcessedPost } from '../interfaces/processed-post';
 import { DashboardService } from '../services/dashboard.service';
@@ -13,7 +9,7 @@ import { PostsService } from '../services/posts.service';
 @Injectable({
   providedIn: 'root'
 })
-export class PostResolver implements Resolve<ProcessedPost[]> {
+export class PostResolver  {
   
   constructor(
     private dashboardService: DashboardService
