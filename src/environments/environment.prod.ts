47fe727e4ed5c2be231d7ade492ab41a9a291292
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-07-23 23:53:12 +0200

    updated slightly the prod file with a comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  //recaptchaPublic: '6LcrwQchAAAAADcgr0bLQ45AknhAQLkzX2RYNavT',
-  maxUploadSize: '250',
+  maxUploadSize: '250', // upload size in mb.
   baseUrl: 'https://app.wafrn.net/api',
   logo: '/assets/logo.png',
   baseMediaUrl: 'https://wafrnmedia.b-cdn.net',
