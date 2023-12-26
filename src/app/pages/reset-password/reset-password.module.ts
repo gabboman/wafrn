ffc8438a603e9748ead7bc6823d9361a52b8f58d
Juan D. Jara <juanorigami@gmail.com>
2023-12-26 16:48:36 +0100

    update reset password form

--- src/app/pages/reset-password/reset-password.module.ts
+++ src/app/pages/reset-password/reset-password.module.ts
@@ -3,23 +3,33 @@ import { CommonModule } from '@angular/common';
 import { ResetPasswordComponent } from './reset-password.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const routes: Routes = [
-      {
-        path: ':email/:resetCode',
-        component: ResetPasswordComponent
-      }
-    ];
+  {
+    path: ':email/:resetCode',
+    component: ResetPasswordComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    ResetPasswordComponent
-  ],
+  declarations: [ResetPasswordComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forChild(routes)
-  ]
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FontAwesomeModule,
+    RouterModule.forChild(routes),
+  ],
 })
-export class ResetPasswordModule { }
+export class ResetPasswordModule {}
