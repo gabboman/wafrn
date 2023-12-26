99daa93b320489c2eb3c78a95c458257af0f88ea
Juan D. Jara <juanorigami@gmail.com>
2023-12-26 22:04:19 +0100

    new nav menu (#48)
    * update vscode ws settings to add format on save
    
    * format and tweak main styles
    
    * clean register component
    
    * format login html
    
    * add proper spacing to register component
    
    * tweak navigation menu
    
    * extract css
    
    * clean login page
    
    * add primeflex dark theme
    
    * refactor recover password page
    
    * update reset password form
    
    * refactor privacy page
    
    * refactor navigation menu
    
    * new nav menu

--- src/app/pages/privacy/privacy.module.ts
+++ src/app/pages/privacy/privacy.module.ts
@@ -2,24 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrivacyComponent } from './privacy.component';
 import { Route, RouterModule } from '@angular/router';
-
-
+import { MatCardModule } from '@angular/material/card';
 
 const routes: Route[] = [
-    {
-      path: '',
-      component: PrivacyComponent
-    }
-  ];
+  {
+    path: '',
+    component: PrivacyComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    PrivacyComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
-
-  ]
+  declarations: [PrivacyComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), MatCardModule],
 })
-export class PrivacyModule { }
+export class PrivacyModule {}
