32b6e49d6b28388b7bcf8a6053526cbbc20911fc
Juan D. Jara <juanorigami@gmail.com>
2023-12-27 23:34:35 +0100

    basic blog page

--- src/app/pages/view-blog/view-blog.component.ts
+++ src/app/pages/view-blog/view-blog.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import {
+  faArrowUpRightFromSquare,
+  faChevronDown,
+  faClockRotateLeft,
+  faHeart,
+  faHeartBroken,
+  faReply,
+  faShareNodes,
+  faTriangleExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 import { Subscription, filter } from 'rxjs';
 import { ProcessedPost } from 'src/app/interfaces/processed-post';
 import { BlocksService } from 'src/app/services/blocks.service';
@@ -14,7 +24,7 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-view-blog',
   templateUrl: './view-blog.component.html',
-  styleUrls: ['./view-blog.component.scss']
+  styleUrls: ['./view-blog.component.scss'],
 })
 export class ViewBlogComponent implements OnInit, OnDestroy {
   loading = true;
@@ -23,6 +33,8 @@ export class ViewBlogComponent implements OnInit, OnDestroy {
   currentPage = 0;
   posts: ProcessedPost[][] = [];
   blogUrl: string = '';
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   blogDetails: any;
   followedUsers: Array<string> = [];
   userLoggedIn = false;
@@ -30,8 +42,17 @@ export class ViewBlogComponent implements OnInit, OnDestroy {
   navigationSubscription!: Subscription;
   showModalTheme = false;
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   splitButtonItems: any[] = [];
 
+  expandDownIcon = faChevronDown;
+  shareIcon = faShareNodes;
+  shareExternalIcon = faArrowUpRightFromSquare;
+  solidHeartIcon = faHeart;
+  clearHeartIcon = faHeartBroken;
+  reblogIcon = faReply;
+  quickReblogIcon = faClockRotateLeft;
+  reportIcon = faTriangleExclamation;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -47,108 +68,132 @@ export class ViewBlogComponent implements OnInit, OnDestroy {
   ) {
     this.userLoggedIn = loginService.checkUserLoggedIn();
   }
+
   ngOnDestroy(): void {
-    if(this.navigationSubscription) {
+    if (this.navigationSubscription) {
       this.navigationSubscription.unsubscribe();
     }
   }
 
-  ngOnInit(): void {
-    this.navigationSubscription = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((ev)=> {
-      this.loading = true;
-      this.found = true;
-      this.viewedPosts = 0;
-      this.currentPage = 0;
-      this.posts = [];
-      this.blogUrl = '';
-      this.avatarUrl = '';
-      this.ngOnInit()
-    })
+  async ngOnInit() {
+    this.navigationSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.loading = true;
+        this.found = true;
+        this.viewedPosts = 0;
+        this.currentPage = 0;
+        this.posts = [];
+        this.blogUrl = '';
+        this.avatarUrl = '';
+        this.ngOnInit();
+      });
+
     this.followedUsers = this.postService.followedUserIds;
-    this.postService.updateFollowers.subscribe( () => {
+    this.postService.updateFollowers.subscribe(() => {
       this.followedUsers = this.postService.followedUserIds;
-    } );
+    });
+
     const blogUrl = this.activatedRoute.snapshot.paramMap.get('url');
-    if(blogUrl) {
+    if (blogUrl) {
       this.blogUrl = blogUrl;
     }
-    this.dashboardService.getBlogDetails(this.blogUrl).then(blogResponse => {
-      if(blogResponse.success === false){
-        this.found = false;
-      } else {
+
+    const blogResponse = await this.dashboardService.getBlogDetails(
+      this.blogUrl
+    );
+
+    if (blogResponse.success === false) {
+      this.found = false;
+    } else {
       this.blogDetails = blogResponse;
       this.splitButtonItems = [
-
-        blogResponse.muted ?
-        {
-          disabled: true,
-          title: 'You muted this user',
-          label: 'You muted this user',
-          icon: 'pi pi-volume-off',
-        } : {
-          title: 'Mute user',
-          label: 'Mute user',
-          command: () => this.blockService.muteUser(this.blogDetails.id).then(() => this.ngOnInit()),
-          icon: 'pi pi-volume-off',
-        },
-        blogResponse.blocked ?
-        {
-          disabled: true,
-          title: 'You blocked this user',
-          label: 'You blocked this user',
-          icon: 'pi pi-ban',
-
-        } : {
-          title: 'Block user',
-          label: 'Block user',
-          command: () => this.blockService.blockUser(this.blogDetails.id).then(() => this.ngOnInit()),
-          icon: 'pi pi-ban',
-
-        },
-        blogResponse.url.startsWith('@') && !blogResponse.serverBlocked ?
-        {
-          title: 'Block server',
-          label: 'Block server',
-          icon: 'pi pi-server',
-          command: () => this.blockService.blockServer(this.blogDetails.id).then(() => this.ngOnInit())
-        } : {
-          disabled: true,
-          visible: blogResponse.url.startsWith('@'),
-          title: `You have blocked this user's server`,
-          label: `You have blocked this user's server`,
-          icon: 'pi pi-server'
-
-        } ,
-      ]
+        blogResponse.muted
+          ? {
+              disabled: true,
+              title: 'You muted this user',
+              label: 'You muted this user',
+              icon: 'pi pi-volume-off',
+            }
+          : {
+              title: 'Mute user',
+              label: 'Mute user',
+              command: () =>
+                this.blockService
+                  .muteUser(this.blogDetails.id)
+                  .then(() => this.ngOnInit()),
+              icon: 'pi pi-volume-off',
+            },
+        blogResponse.blocked
+          ? {
+              disabled: true,
+              title: 'You blocked this user',
+              label: 'You blocked this user',
+              icon: 'pi pi-ban',
+            }
+          : {
+              title: 'Block user',
+              label: 'Block user',
+              command: () =>
+                this.blockService
+                  .blockUser(this.blogDetails.id)
+                  .then(() => this.ngOnInit()),
+              icon: 'pi pi-ban',
+            },
+        blogResponse.url.startsWith('@') && !blogResponse.serverBlocked
+          ? {
+              title: 'Block server',
+              label: 'Block server',
+              icon: 'pi pi-server',
+              command: () =>
+                this.blockService
+                  .blockServer(this.blogDetails.id)
+                  .then(() => this.ngOnInit()),
+            }
+          : {
+              disabled: true,
+              visible: blogResponse.url.startsWith('@'),
+              title: `You have blocked this user's server`,
+              label: `You have blocked this user's server`,
+              icon: 'pi pi-server',
+            },
+      ];
       this.loadPosts(this.currentPage).then(() => {
         this.loading = false;
       });
-      this.avatarUrl = this.blogDetails.url.startsWith('@') ? environment.externalCacheurl + encodeURIComponent(this.blogDetails.avatar) : environment.baseMediaUrl + this.blogDetails.avatar
-      this.titleService.setTitle(`${this.blogDetails.url}\'s blog`);
-        this.metaTagService.addTags([
-          {name: 'description', content: `${this.blogDetails.url}\'s wafrn blog`},
-          {name: 'author', content: this.blogDetails.url },
-          {name: 'image', content: this.avatarUrl}
-        ]);
+      this.avatarUrl = this.blogDetails.url.startsWith('@')
+        ? environment.externalCacheurl +
+          encodeURIComponent(this.blogDetails.avatar)
+        : environment.baseMediaUrl + this.blogDetails.avatar;
+      this.titleService.setTitle(`${this.blogDetails.url}'s blog`);
+      this.metaTagService.addTags([
+        {
+          name: 'description',
+          content: `${this.blogDetails.url}'s wafrn blog`,
+        },
+        { name: 'author', content: this.blogDetails.url },
+        { name: 'image', content: this.avatarUrl },
+      ]);
+    }
+
+    const userHasCustomTheme = await this.themeService.checkThemeExists(
+      this.blogDetails.id
+    );
+
+    if (userHasCustomTheme) {
+      const userResponseToCustomThemes =
+        this.themeService.hasUserAcceptedCustomThemes();
+
+      if (userResponseToCustomThemes === 2) {
+        this.themeService.setTheme(this.blogDetails.id);
       }
-      this.themeService.checkThemeExists(this.blogDetails.id).then((userHasCustomTheme) => {
-        if(userHasCustomTheme){
-          const userResponseToCustomThemes = this.themeService.hasUserAcceptedCustomThemes();
-          if ( userResponseToCustomThemes === 2) {
-            this.themeService.setTheme(this.blogDetails.id)
-          }
 
-          if (userResponseToCustomThemes === 0) {
-            this.showModalTheme = true;
-          }
-        } else {
-          this.themeService.setTheme('');
-        }
-      })
-    })
-
-
-
+      if (userResponseToCustomThemes === 0) {
+        this.showModalTheme = true;
+      }
+    } else {
+      this.themeService.setTheme('');
+    }
   }
 
   async countViewedPost() {
@@ -160,38 +205,52 @@ export class ViewBlogComponent implements OnInit, OnDestroy {
   }
 
   async loadPosts(page: number) {
-    const tmpPosts = await this.dashboardService.getBlogPage(page, this.blogUrl);
-    tmpPosts.forEach(post => {
+    const tmpPosts = await this.dashboardService.getBlogPage(
+      page,
+      this.blogUrl
+    );
+    tmpPosts.forEach((post) => {
       this.posts.push(post);
     });
   }
 
   async unfollowUser(id: string) {
     const response = await this.postService.unfollowUser(id);
-    if(response) {
-      this.messages.add({ severity: 'success', summary: 'You no longer follow this user!' });
+    if (response) {
+      this.messages.add({
+        severity: 'success',
+        summary: 'You no longer follow this user!',
+      });
     } else {
-      this.messages.add({ severity: 'error', summary: 'Something went wrong! Check your internet conectivity and try again' });
+      this.messages.add({
+        severity: 'error',
+        summary:
+          'Something went wrong! Check your internet conectivity and try again',
+      });
     }
-
   }
 
   async followUser(id: string) {
     const response = await this.postService.followUser(id);
-    if(response) {
-      this.messages.add({ severity: 'success', summary: 'You now follow this user!' });
+    if (response) {
+      this.messages.add({
+        severity: 'success',
+        summary: 'You now follow this user!',
+      });
     } else {
-      this.messages.add({ severity: 'error', summary: 'Something went wrong! Check your internet conectivity and try again' });
+      this.messages.add({
+        severity: 'error',
+        summary:
+          'Something went wrong! Check your internet conectivity and try again',
+      });
     }
   }
-
 
   answerCustomThemeModal(response: number) {
     localStorage.setItem('acceptsCustomThemes', response.toString());
     if (response === 2) {
-      this.themeService.setTheme(this.blogDetails.id)
+      this.themeService.setTheme(this.blogDetails.id);
     }
     this.showModalTheme = false;
   }
-
 }
--- src/app/pages/view-blog/view-blog.module.ts
+++ src/app/pages/view-blog/view-blog.module.ts
@@ -5,26 +5,32 @@ import { ViewBlogComponent } from './view-blog.component';
 import { PostModule } from '../../components/post/post.module';
 import { PagenotfoundModule } from '../pagenotfound/pagenotfound.module';
 import { DeferModule } from 'src/app/directives/defer/defer.module';
-
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatMenuModule } from '@angular/material/menu';
 
 const routes: Routes = [
-      {
-        path: ':url',
-        component: ViewBlogComponent
-      }
-    ];
+  {
+    path: ':url',
+    component: ViewBlogComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    ViewBlogComponent
-    ],
+  declarations: [ViewBlogComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     DeferModule,
     PostModule,
     PagenotfoundModule,
-  ]
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatButtonModule,
+    FontAwesomeModule,
+    MatMenuModule,
+  ],
 })
-export class ViewBlogModule { }
+export class ViewBlogModule {}
