eb9145e1b7ff19f451e21e4414ff240418b0ae2d
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-12-31 14:47:55 +0000

    ugh serviceworkers were a mistake

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
   maxUploadSize: '250', // upload size in mb.
-  baseUrl: 'https://api.wafrn.net/api',
+  //baseUrl: 'https://api.wafrn.net/api',
+  baseUrl: 'https://app.wafrn.net/api',
   logo: '/assets/logo.png',
   //baseMediaUrl: 'https://wafrnmedia.b-cdn.net',
   baseMediaUrl: 'https://media.wafrn.net',
   //externalCacheurl: 'https://wafrncache.b-cdn.net/?media=',
-  externalCacheurl: 'https://cache.wafrn.net/?media=',
-  //externalCacheurl: 'https://app.wafrn.net/api/cache?media=',
+  //externalCacheurl: 'https://cache.wafrn.net/?media=',
+  externalCacheurl: 'https://app.wafrn.net/api/cache?media=',
   frontUrl: 'https://app.wafrn.net',
   shortenPosts: 5,
 };
