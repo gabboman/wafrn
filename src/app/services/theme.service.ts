ec6c084740652e44578056f4e0aa114e80f42efc
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-12-31 13:42:16 +0000

    temporarily disabled css theme

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -5,107 +5,115 @@ import { HttpClient } from '@angular/common/http';
 import { UtilsService } from './utils.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
-
   constructor(
     private loginService: LoginService,
     private http: HttpClient,
-    private utils: UtilsService,
+    private utils: UtilsService
+  ) {}
 
-    ) { }
+  setMyTheme() {
+    // TODO re-enable themes
+    //this.setTheme(this.loginService.getLoggedUserUUID())
+  }
 
-    setMyTheme(){
-      this.setTheme(this.loginService.getLoggedUserUUID())
-    }
+  updateTheme(newTheme: string) {
+    return this.http
+      .post(`${environment.baseUrl}/updateCSS`, { css: newTheme })
+      .toPromise();
+  }
 
-    updateTheme(newTheme: string) {
-      return this.http.post(`${environment.baseUrl}/updateCSS`, {css: newTheme}).toPromise()
-    }
+  // 0 no data 1 does not want custom css 2 accepts custom css
+  hasUserAcceptedCustomThemes(): number {
+    let res = 0;
+    try {
+      const storedResponse = localStorage.getItem('acceptsCustomThemes');
+      res = storedResponse ? parseInt(storedResponse) : 0;
+    } catch (error) {}
+    return res;
+  }
 
-    // 0 no data 1 does not want custom css 2 accepts custom css
-    hasUserAcceptedCustomThemes(): number{
-      let res = 0;
-      try {
-        const storedResponse = localStorage.getItem('acceptsCustomThemes')
-        res = storedResponse ? parseInt(storedResponse) : 0
-      } catch (error){
+  async checkThemeExists(theme: string): Promise<boolean> {
+    let res = false;
+    try {
+      const response = await this.http
+        .get(`${environment.baseMediaUrl}/themes/${theme}.css`, {
+          responseType: 'text',
+        })
+        .toPromise();
+      if (response && response.length > 0) {
+        res = true;
       }
-      return res;
-    }
+    } catch (error) {}
+    return res;
+  }
 
-    async checkThemeExists(theme: string): Promise<boolean> {
-      let res = false;
-      try {
-        const response = await this.http.get( `${environment.baseMediaUrl}/themes/${theme}.css`, { responseType: 'text' }).toPromise()
-        if(response && response.length > 0) {
-          res = true;
-        }
-      } catch (error) {
-
+  async getMyThemeAsSting(): Promise<string> {
+    let res = '';
+    try {
+      const themeResponse = await this.http
+        .get(
+          `${
+            environment.baseUrl
+          }/uploads/themes/${this.loginService.getLoggedUserUUID()}.css`,
+          { responseType: 'text' }
+        )
+        .toPromise();
+      if (themeResponse && themeResponse.length > 0) {
+        res = themeResponse;
       }
-      return res;
+    } catch (error) {}
+    return res;
+  }
+
+  setTheme(themeToSet: string) {
+    this.setStyle(
+      'customUserTheme',
+      `${environment.baseUrl}/uploads/themes/${themeToSet}.css`
+    );
+  }
+
+  private getLinkElementForKey(key: string) {
+    return (
+      this.getExistingLinkElementByKey(key) ||
+      this.createLinkElementWithKey(key)
+    );
+  }
+
+  private getExistingLinkElementByKey(key: string) {
+    return document.head.querySelector(
+      `link[rel="stylesheet"].${this.getClassNameForKey(key)}`
+    );
+  }
+
+  private createLinkElementWithKey(key: string) {
+    const linkEl = document.createElement('link');
+    linkEl.setAttribute('rel', 'stylesheet');
+    linkEl.classList.add(this.getClassNameForKey(key));
+    document.head.appendChild(linkEl);
+    return linkEl;
+  }
+
+  private getClassNameForKey(key: string) {
+    return `app-${key}`;
+  }
+
+  /**
+   * Set the stylesheet with the specified key.
+   */
+  private setStyle(key: string, href: string) {
+    this.getLinkElementForKey(key).setAttribute('href', href);
+  }
+
+  /**
+   * Remove the stylesheet with the specified key.
+   */
+  private removeStyle(key: string) {
+    const existingLinkElement = this.getExistingLinkElementByKey(key);
+    if (existingLinkElement) {
+      document.head.removeChild(existingLinkElement);
     }
-
-    async getMyThemeAsSting(): Promise<string>{
-      let res = '';
-      try {
-        const themeResponse = await this.http.get( `${environment.baseUrl}/uploads/themes/${this.loginService.getLoggedUserUUID()}.css`, { responseType: 'text' }).toPromise()
-        if(themeResponse && themeResponse.length > 0) {
-          res = themeResponse;
-        }
-      } catch (error) {
-
-      }
-      return res;
-    }
-
-
-
-    setTheme(themeToSet: string) {
-      this.setStyle(
-        "customUserTheme",
-        `${environment.baseUrl}/uploads/themes/${themeToSet}.css`
-      );
-    }
-
-    private getLinkElementForKey(key: string) {
-      return this.getExistingLinkElementByKey(key) || this.createLinkElementWithKey(key);
-    }
-
-    private getExistingLinkElementByKey(key: string) {
-      return document.head.querySelector(
-        `link[rel="stylesheet"].${this.getClassNameForKey(key)}`
-      );
-    }
-
-    private createLinkElementWithKey(key: string) {
-      const linkEl = document.createElement("link");
-      linkEl.setAttribute("rel", "stylesheet");
-      linkEl.classList.add(this.getClassNameForKey(key));
-      document.head.appendChild(linkEl);
-      return linkEl;
-    }
-
-    private getClassNameForKey(key: string) {
-      return `app-${key}`;
-    }
-
-    /**
-     * Set the stylesheet with the specified key.
-     */
-    private setStyle(key: string, href: string) {
-      this.getLinkElementForKey(key).setAttribute("href", href);
-    }
-
-    /**
-     * Remove the stylesheet with the specified key.
-     */
-    private removeStyle(key: string) {
-      const existingLinkElement = this.getExistingLinkElementByKey(key);
-      if (existingLinkElement) {
-        document.head.removeChild(existingLinkElement);
-      }
-    }
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,9 @@ export const environment = {
   logo: '/assets/logo.png',
   //baseMediaUrl: 'https://wafrnmedia.b-cdn.net',
   baseMediaUrl: 'https://media.wafrn.net',
-  //externalCacheurl: 'https://wafrncache.b-cdn.net/?media=',
+  externalCacheurl: 'https://wafrncache.b-cdn.net/?media=',
   //externalCacheurl: 'https://cache.wafrn.net/?media=',
-  externalCacheurl: 'https://app.wafrn.net/api/cache?media=',
+  //externalCacheurl: 'https://app.wafrn.net/api/cache?media=',
   frontUrl: 'https://app.wafrn.net',
   shortenPosts: 5,
 };
