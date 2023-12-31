3599fd1c5b6efaad984468352a6fccd6b8d6cf0c
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-12-31 15:47:29 +0000

    better edit profile

--- src/app/pages/profile/edit-profile/edit-profile.module.ts
+++ src/app/pages/profile/edit-profile/edit-profile.module.ts
@@ -6,26 +6,28 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { loginRequiredGuard } from 'src/app/guards/login-required.guard';
 import { QuillModule } from 'ngx-quill';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 const routes: Routes = [
-      {
-        path: '',
-        component: EditProfileComponent,
-        canActivate: [loginRequiredGuard]
-      }
-    ];
+  {
+    path: '',
+    component: EditProfileComponent,
+    canActivate: [loginRequiredGuard],
+  },
+];
 
 @NgModule({
-  declarations: [
-    EditProfileComponent
-  ],
+  declarations: [EditProfileComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     FormsModule,
     ReactiveFormsModule,
     QuillModule,
-    MatCardModule
-  ]
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
 })
-export class EditProfileModule { }
+export class EditProfileModule {}
