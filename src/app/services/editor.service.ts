e32aa28e8a0c0396e39f28a93c1e72e7acb31206
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-01-11 00:16:28 +0100

    formated

--- src/app/services/editor.service.ts
+++ src/app/services/editor.service.ts
@@ -7,29 +7,32 @@ import { ProcessedPost } from '../interfaces/processed-post';
 import { Action, EditorLauncherData } from '../interfaces/editor-launcher-data';
 
 @Injectable({
-  providedIn: 'any'
+  providedIn: 'any',
 })
 export class EditorService {
-
   base_url = environment.baseUrl;
-  public launchPostEditorEmitter: BehaviorSubject<EditorLauncherData> = new BehaviorSubject<EditorLauncherData>({
-    action: Action.None
-  });
+  public launchPostEditorEmitter: BehaviorSubject<EditorLauncherData> =
+    new BehaviorSubject<EditorLauncherData>({
+      action: Action.None,
+    });
 
-
-  constructor(
-    private http: HttpClient,
-  ) {
-    this.launchPostEditorEmitter.subscribe((data)=> {
+  constructor(private http: HttpClient) {
+    this.launchPostEditorEmitter.subscribe((data) => {
       if (data.action !== Action.None) {
         this.launchPostEditorEmitter.next({
-          action: Action.None
-        })
+          action: Action.None,
+        });
       }
-    })
+    });
   }
 
-  async createPost(content: string,privacy: number, tags?: string, idPostToReblog?: string, contentWarning?: string): Promise<boolean> {
+  async createPost(
+    content: string,
+    privacy: number,
+    tags?: string,
+    idPostToReblog?: string,
+    contentWarning?: string
+  ): Promise<boolean> {
     let success: boolean = false;
     try {
       const formdata = {
@@ -37,28 +40,33 @@ export class EditorService {
         parent: idPostToReblog,
         tags: tags,
         privacy: privacy,
-        content_warning: contentWarning ? contentWarning : ''
+        content_warning: contentWarning ? contentWarning : '',
       };
-      const petitionResponse: any = await this.http.post(`${this.base_url}/createPost`, formdata).toPromise();
+      const petitionResponse: any = await this.http
+        .post(`${this.base_url}/createPost`, formdata)
+        .toPromise();
       success = petitionResponse.id;
-
     } catch (exception) {
       console.log(exception);
     }
 
-
     return success;
   }
 
-  async uploadMedia(description: string, nsfw: boolean, img: File): Promise<WafrnMedia | undefined > {
+  async uploadMedia(
+    description: string,
+    nsfw: boolean,
+    img: File
+  ): Promise<WafrnMedia | undefined> {
     let res: WafrnMedia | undefined = undefined;
     try {
-      const payload =  new FormData();
+      const payload = new FormData();
       payload.append('files', img);
       payload.append('description', description);
       payload.append('nsfw', nsfw.toString());
-      const petition: any = await this.http.post<Array<WafrnMedia>>(`${environment.baseUrl}/uploadMedia`,
-       payload).toPromise();
+      const petition: any = await this.http
+        .post<Array<WafrnMedia>>(`${environment.baseUrl}/uploadMedia`, payload)
+        .toPromise();
       if (petition) {
         res = petition[0];
       }
@@ -70,11 +78,15 @@ export class EditorService {
   }
 
   async searchUser(url: string) {
-    return await this.http.get(`${environment.baseUrl}/userSearch/${encodeURIComponent(url)}`).toPromise();
+    return await this.http
+      .get(`${environment.baseUrl}/userSearch/${encodeURIComponent(url)}`)
+      .toPromise();
   }
 
   async getEditorComponent(): Promise<typeof PostEditorComponent> {
-    const { PostEditorComponent } = await import("../components/post-editor/post-editor.component");
-    return PostEditorComponent
+    const { PostEditorComponent } = await import(
+      '../components/post-editor/post-editor.component'
+    );
+    return PostEditorComponent;
   }
 }
