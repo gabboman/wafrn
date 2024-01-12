c1581b916c174d1cae7cb307dfc4f365a0b50179
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-01-12 22:58:27 +0100

    Server list component

--- src/app/pages/admin/server-list/server-list.component.ts
+++ src/app/pages/admin/server-list/server-list.component.ts
@@ -1,42 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { server } from 'src/app/interfaces/servers';
 import { AdminService } from 'src/app/services/admin.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-server-list',
   templateUrl: './server-list.component.html',
-  styleUrls: ['./server-list.component.scss']
+  styleUrls: ['./server-list.component.scss'],
 })
-export class ServerListComponent {
+export class ServerListComponent implements OnInit {
   ready = false;
-  servers: server[] = []
-  originalServers: server[] = []
+  originalServers: server[] = [];
 
-  constructor(
-    private adminService: AdminService
-  ) {
-    this.adminService.getServers().then((response)=> {
-      this.servers = response;
+  displayedColumns = ['displayName', 'blocked', 'detail'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  dataSource!: MatTableDataSource<server, MatPaginator>;
+
+  constructor(private adminService: AdminService) {
+    this.adminService.getServers().then((response) => {
       this.originalServers = JSON.parse(JSON.stringify(response));
       this.ready = true;
-      console.log(this.servers)
-    })
+      this.dataSource.data = response;
+      console.log(response);
+    });
   }
-
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<server, MatPaginator>([]);
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 
   async save() {
     // we detect where we have made changes the shotgun way
     const serversToPatch: server[] = [];
-    this.servers.forEach((elem, index) => {
-      const original = this.originalServers[index]
-      if(elem.blocked != original.blocked || elem.detail != original.detail) {
-        serversToPatch.push(elem)
+    this.dataSource.data.forEach((elem, index) => {
+      const original = this.originalServers[index];
+      if (elem.blocked != original.blocked || elem.detail != original.detail) {
+        serversToPatch.push(elem);
       }
     });
     await this.adminService.updateServers(serversToPatch);
-    this.originalServers = JSON.parse(JSON.stringify(this.servers));
-
-
+    this.originalServers = JSON.parse(JSON.stringify(this.dataSource.data));
   }
-
 }
--- src/app/pages/admin/server-list/server-list.module.ts
+++ src/app/pages/admin/server-list/server-list.module.ts
@@ -6,6 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 const routes: Route[] = [
   {
@@ -23,6 +26,9 @@ const routes: Route[] = [
     MatTableModule,
     MatCardModule,
     MatPaginatorModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
   ],
 })
 export class ServerListModule {}
