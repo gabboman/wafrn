43d937e050da1d44cd196c2cef5153e9fe0e8b38
FireIsGood <fireisadequate@gmail.com>
2024-02-13 12:39:30 -0800

    Lint config files

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-while true
-do
-    git pull origin main
-    npm run build:ssr
-    npm run serve:ssr
+while true; do
+  git pull origin main
+  npm run build:ssr
+  npm run serve:ssr
 done
