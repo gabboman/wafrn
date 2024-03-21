91e7b42e43c29247ac4ba6b9badeae2f3bb12357
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-03-22 00:31:47 +0100

    EMOJIS

--- src/app/components/post-fragment/post-fragment.component.ts
+++ src/app/components/post-fragment/post-fragment.component.ts
@@ -7,6 +7,8 @@ import { RouterModule } from '@angular/router';
 import { InjectHtmlModule } from 'src/app/directives/inject-html/inject-html.module';
 import { MatButtonModule } from '@angular/material/button';
 import { SimplifiedUser } from 'src/app/interfaces/simplified-user';
+import { environment } from 'src/environments/environment';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-post-fragment',
@@ -18,6 +20,7 @@ import { SimplifiedUser } from 'src/app/interfaces/simplified-user';
     RouterModule,
     InjectHtmlModule,
     MatButtonModule,
+    MatTooltipModule,
   ],
   templateUrl: './post-fragment.component.html',
   styleUrl: './post-fragment.component.scss',
@@ -27,13 +30,36 @@ export class PostFragmentComponent implements OnInit {
   @Input() showCw: boolean = true;
   @Output() dismissCw: EventEmitter<void> = new EventEmitter<void>();
   emojiCollection: {
+    id: string;
     content: string;
-    img: string;
+    img?: string;
     users: SimplifiedUser[];
   }[] = [];
   ngOnInit(): void {
-    if(this.fragment?.emojiReactions) {
-
+    if (this.fragment?.emojiReactions) {
+      // this is n^2 but fuck this we expect the emojis to be small. and no, i am not adding _ to wafrn again
+      this.fragment.emojiReactions.forEach((reaction) => {
+        if (!this.emojiCollection.find((col) => col.id === reaction.emojiId)) {
+          this.emojiCollection.push({
+            id: reaction.emojiId,
+            content: reaction.content,
+            img: reaction.emoji?.url
+              ? environment.externalCacheurl +
+                encodeURIComponent(reaction.emoji.url)
+              : undefined,
+            users: this.fragment?.emojiReactions
+              .filter(
+                (reactToFilter) => reactToFilter.emojiId === reaction.emojiId
+              )
+              .map((reac) => reac.user)
+              .filter((usr) => usr?.avatar) as SimplifiedUser[],
+          });
+        }
+      });
     }
+  }
+
+  getTooltipUsers(users: SimplifiedUser[]): string {
+    return users.map((usr) => usr.url).join(',');
   }
 }
