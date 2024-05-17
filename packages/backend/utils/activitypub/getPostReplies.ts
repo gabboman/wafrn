ab794869d51494c1c05955839eeab4d32761d560
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-05-18 00:09:39 +0200

    Improved privacy of getpostreplies

--- packages/backend/utils/activitypub/getPostReplies.ts
+++ packages/backend/utils/activitypub/getPostReplies.ts
@@ -1,11 +1,15 @@
+import { Op } from "sequelize"
 import { Post } from "../../db"
 import { environment } from "../../environment"
 
 async function getPostReplies(postId: string) {
     const posts = await Post.findAll({
-        attributes: ['id', 'parentId', 'remotePostId'],
+        attributes: ['id', 'parentId', 'remotePostId', 'privacy'],
         where: {
-            parentId: postId
+            parentId: postId,
+            privacy: {
+                [Op.notIn]: [2, 10] 
+            }
         }
     })
     return posts.map((elem: any) => elem.remotePostId ? elem.remotePostId : `${environment.frontendUrl}/fediverse/post/${elem.id}`)
