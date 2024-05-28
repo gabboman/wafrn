02d49e0caf80e2d1cd295d318a85b8affc670d2b
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-05-28 15:24:11 +0200

    blog preview

--- packages/backend/routes/frontend.ts
+++ packages/backend/routes/frontend.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express'
 import { environment } from '../environment'
 import { Op } from 'sequelize'
-import { Media, Post, User } from '../db'
+import { Media, Post, User, sequelize } from '../db'
 import fs from 'fs'
 import * as DOMPurify from 'isomorphic-dompurify'
 import { redisCache } from '../utils/redis'
@@ -20,7 +20,6 @@ export default function frontend(app: Application) {
       '/',
       '/index.html',
       '/index',
-      '/blog/*',
       '/dashboard/*',
       '/dashboard',
       '/login',
@@ -40,6 +39,23 @@ export default function frontend(app: Application) {
         const postData = await getPostSEOCache(req.params.id)
         if (postData) {
           res.send(getIndexSeo(postData.title, postData.description, postData.img))
+        } else {
+          res.send(getIndexSeo(defaultSeoData.title, defaultSeoData.description, defaultSeoData.img))
+        }
+      } catch (error) {
+        res.send(getIndexSeo(defaultSeoData.title, defaultSeoData.description, defaultSeoData.img))
+      }
+    } else {
+      res.send(getIndexSeo(defaultSeoData.title, defaultSeoData.description, defaultSeoData.img))
+    }
+  })
+
+  app.get('/blog/:url', async function (req, res) {
+    if (req.params?.url) {
+      try {
+        const blogData = await getBlogSEOCache(req.params.url)
+        if (blogData) {
+          res.send(getIndexSeo(blogData.title, blogData.description, blogData.img))
         } else {
           res.send(getIndexSeo(defaultSeoData.title, defaultSeoData.description, defaultSeoData.img))
         }
@@ -95,6 +111,32 @@ async function getPostSEOCache(id: string): Promise<{ title: string; description
       const safeMedia = post.medias?.find((elem: any) => elem.NSFW === false && !elem.url.toLowerCase().endsWith('mp4'))
       res.img = safeMedia?.url
       redisCache.set('postSeoCache:' + id, JSON.stringify(res), 'EX', 300)
+    }
+  } else {
+    res = JSON.parse(resData)
+  }
+  return res
+}
+
+async function getBlogSEOCache(url: string): Promise<{ title: string; description: string; img: string }> {
+  const resData = await redisCache.get('blogSeoCache:' + url)
+  let res = { ...environment.defaultSEOData }
+  if (!resData) {
+    const blog = await User.findOne({
+      where: {
+        url: sequelize.where(sequelize.fn('LOWER', sequelize.col('url')), 'LIKE', (url).toLowerCase())
+      }
+    })
+    if(blog) {
+      const url = sanitizeStringForSEO(blog.url).substring(0, 65)
+      const description = sanitizeStringForSEO(blog.description).substring(0, 200)
+      res.title = blog.url.startsWith('@') ? `Blog from external user ${url}`: `@${url}'s wafrn blog`
+      res.description = description
+      res.img = blog.url.startsWith('@') ? blog.avatar : `${environment.mediaUrl}${blog.avatar}`
+      await redisCache.set('blogSeoCache:' + url, JSON.stringify(res), 'EX', 300)
+    } else {
+      // Given the fact that query can be slow even if the blog does not exist, we should cache the 404
+      await redisCache.set('blogSeoCache:' + url, JSON.stringify(res), 'EX', 600)
     }
   } else {
     res = JSON.parse(resData)
