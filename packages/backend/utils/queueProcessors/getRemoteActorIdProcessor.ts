66155725d22ccc6551543bdd099edb35bc460ee7
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-06-10 22:00:00 +0200

    fix crash

--- packages/backend/utils/queueProcessors/getRemoteActorIdProcessor.ts
+++ packages/backend/utils/queueProcessors/getRemoteActorIdProcessor.ts
@@ -229,7 +229,7 @@ async function getRemoteActorIdProcessor(job: Job) {
           logger.info({
             message: `Error processing emojis from user ${userRes.url}`,
             error: error,
-            emojis: userPetition?.tag?.filter((elem: fediverseTag) => elem.type === 'Emoji'),
+            emojis: userPetition?.tag ? userPetition.tag.filter((elem: fediverseTag) => elem.type === 'Emoji') : 'undefined',
             userPetition: userPetition
           })
         }
