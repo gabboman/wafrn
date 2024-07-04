67067848a57ebd1877d1343e5cde4b02fbb9655e
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-07-04 17:47:23 +0200

    remove tolowercase that is slowing down stuff

--- packages/backend/utils/cacheGetters/getUserIdFromRemoteId.ts
+++ packages/backend/utils/cacheGetters/getUserIdFromRemoteId.ts
@@ -18,7 +18,7 @@ async function getUserIdFromRemoteId(remoteId: string): Promise<string> {
       : await User.findOne({
           attributes: ['id'],
           where: {
-            remoteId: sequelize.where(sequelize.fn('LOWER', sequelize.col('remoteId')), '=', remoteId.toLowerCase())
+            remoteId: remoteId
           }
         })
     if (user) {
