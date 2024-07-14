834b8b82bb0dd677100052fe25d83a9fb718cbd4
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-07-14 16:43:31 +0200

    Fixed forcing signature when not needed

--- packages/backend/utils/checkUserAllowsThreads.ts
+++ packages/backend/utils/checkUserAllowsThreads.ts
@@ -6,6 +6,8 @@ async function checkuserAllowsThreads(req: SignedRequest, user: any) {
         const options = await getUserOptions((await user).id)
         const userFederatesWithThreads = options.filter(elem => elem.optionName === 'wafrn.federateWithThreads' && elem.optionValue === 'true')
         return userFederatesWithThreads.length > 0
+      } else {
+        return true
       }
 }
 
