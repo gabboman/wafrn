26602d16e58b58b45e56082f46dc338fb66af09c
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-07-23 17:37:02 +0200

    cleaned imports

--- packages/backend/remoteCache.ts
+++ packages/backend/remoteCache.ts
@@ -1,12 +1,7 @@
 import express, { Response } from 'express'
 import cors from 'cors'
-import { activityPubRoutes } from './routes/activitypub/activitypub'
-import { wellKnownRoutes } from './routes/activitypub/well-known'
 import { environment } from './environment'
-import overrideContentType from './utils/overrideContentType'
 import { logger } from './utils/logger'
-import bodyParser from 'body-parser'
-import { SignedRequest } from './interfaces/fediverse/signedRequest'
 import cacheRoutes from './routes/remoteCache'
 
 
