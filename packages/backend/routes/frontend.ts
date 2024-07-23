8cdc33d40e4b92fe73df4f83a64fcc4e64d7f923
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-07-23 21:57:26 +0200

    add get post endpoint to frontend

--- packages/backend/routes/frontend.ts
+++ packages/backend/routes/frontend.ts
@@ -68,6 +68,34 @@ function frontend(app: Application) {
 
   // serve static angular files
   app.get('*.*', express.static(environment.frontedLocation, cacheOptions))
+
+  app.get(
+    ['/fediverse/post/:id', '/fediverse/activity/post/:id'],
+    getCheckFediverseSignatureFucnction(false),
+    async (req: SignedRequest, res: Response) => {
+      if (
+        req.fediData?.valid
+      ) {
+        await handlePostRequest(req, res)
+      } else {
+        const defaultSeoData = environment.defaultSEOData
+        if (req.params?.id) {
+          try {
+            const postData = await getPostSEOCache(req.params.id)
+            if (postData) {
+              res.send(getIndexSeo(postData.title, postData.description, postData.img))
+            } else {
+              res.send(getIndexSeo(defaultSeoData.title, defaultSeoData.description, defaultSeoData.img))
+            }
+          } catch (error) {
+            res.send(getIndexSeo(defaultSeoData.title, defaultSeoData.description, defaultSeoData.img))
+          }
+        } else {
+          res.send(getIndexSeo(defaultSeoData.title, defaultSeoData.description, defaultSeoData.img))
+        }
+      }
+    }
+  )
 }
 
 function sanitizeStringForSEO(unsanitized: string): string {
