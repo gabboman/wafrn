784434bbc3a92e0f8e2de0cb4051a6b322659af8
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-07-29 16:14:28 +0200

    fix follows not sent

---
+++ packages/backend/utils/maintenanceTasks/fixFollowsNotSent.ts
@@ -0,0 +1,35 @@
+import { Op } from "sequelize";
+import { Follows } from "../../db";
+import { getAllLocalUserIds } from "../cacheGetters/getAllLocalUserIds";
+import { acceptRemoteFollow } from "../activitypub/acceptRemoteFollow";
+
+
+async function fix() {
+    const localUsers = await getAllLocalUserIds()
+    console.log(localUsers)
+    const follows = await Follows.findAll({
+        where: {
+            accepted: true,
+            followedId: {
+                [Op.in]: localUsers
+            },
+            followerId: {
+                [Op.notIn]: localUsers
+            }
+        }
+    })
+    for await (const follow of follows) {
+        try {
+            await acceptRemoteFollow(follow.followedId, follow.followerId)
+            console.log(`user ${follow.followedId} followed ${follow.followerId}`)
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+}
+
+
+fix().then(()=> {
+    console.log('done')
+})
