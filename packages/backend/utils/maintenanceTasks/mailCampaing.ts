d78966ecd85c999833e22b005b164f5ecc658bc1
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-08-06 22:43:44 +0200

    new mail campaign

--- packages/backend/utils/maintenanceTasks/mailCampaing.ts
+++ packages/backend/utils/maintenanceTasks/mailCampaing.ts
@@ -5,7 +5,7 @@ import sendActivationEmail from '../sendActivationEmail'
 
 async function sendMail() {
   const users = await User.findAll({
-    attributes: ['url', 'email'],
+    attributes: ['url', 'email', 'banned', 'activated', 'disableEmailNotifications'],
     where: {
       banned: { [Op.ne]: true },
       activated: true,
@@ -18,15 +18,15 @@ async function sendMail() {
   })
 
   for await (const user of users) {
-    const subject = `Hello ${user.url}, we have updated the interface. AGAIN! Now is rounded and bubbly`
+    const subject = `Hello ${user.url}, you now can set your account to manually accept new followers! Also minecraft server`
     const body = `
-    <h1>What's up ${user.url}, hope all is ok and going very evil-ly!</h1>
-<p>We have made a huge update to <a href="https://app.wafrn.net" target="_blank">wafrn</a>, and other cool stuff</p>
+    <h1>What's up ${user.url}?</h1>
+<p>We have made a huge update to <a href="https://app.wafrn.net" target="_blank">wafrn</a>! Now you can set your account to manually accept new follow requests! And not only that, you can remove people who you did not want to follow you in the first place!</p>
 <a href="https://app.wafrn.net"><img style="width:100%" src="https://app.wafrn.net/assets/logo.png" /> </a>
-<p>Do not worry about your theme! This time, unlike on christmas, old themes will still be working! Think of this update of a new base theme!</p>
-<p>We have a new feature too! This one please read the whole pagraph before you get angry!</p>
-<p>You may know of threads, meta/facebook fediverse software. Well, <b>if you chose</b>, you can federate with them. You have to activate it in your settings to be able to follow people in threads. If you dont chose to, is like wafrn had them blocked. Your information is safe from them</p>
-<p>There are good reasons both to not want to federate with them and to do it. But making this opt out would be disrispecting you.</p>
+<img src="https://media.wafrn.net/1722976767875_0df27e8ad3c5057cd578d3376d9815ea6ca26a0e.webp" />
+<p>We have also updated the post view! Now you can see replies to your dearranged posts in a more easy way!</p>
+<img src="https://media.wafrn.net/1722976907776_f95f6a00fa74b34df7ded82f915909afcad30abc.webp" />
+<p>Oh, almost forgot, as you can see we also have another minecraft server. Go join app.wafrn.net, you can use bedrock or java. Do not worry, its compatible with old versions and modern ones! :D</p>
 <p>Also, if you have one spare buck, give it to me! I want to invite my wife to a pizza. We have <a href="https://patreon.com/wafrn">patreon</a> and <a href="https://ko-fi.com/wafrn">ko-fi</a>. You dont have to, but it would be cool</p>
 <h6>Attention, we might go for something healthier instead of pizza</h6>
 <h2>Come back to <a href="https://app.wafrn.net">wafrn</a>, we miss you!</h2>
