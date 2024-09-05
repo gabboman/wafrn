3fc73dd8b3f0f7a41b69f17b4550c95e3774764d
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-09-05 22:55:50 +0200

    added AAC TTS for friend

--- packages/frontend/src/app/pages/aac-for-alex/aac-for-alex.component.ts
+++ packages/frontend/src/app/pages/aac-for-alex/aac-for-alex.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-aac-for-alex',
@@ -9,34 +13,50 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   templateUrl: './aac-for-alex.component.html',
   styleUrl: './aac-for-alex.component.scss'
 })
 export class AacForAlexComponent implements OnInit {
 
-  text  = "loading"
-  voice : SpeechSynthesisVoice | undefined
-  ngOnInit(): void {
-    const voices = speechSynthesis.getVoices().filter(elem => elem.lang == "nl-NL") ;
-    if(voices.length > 0) {
-      this.text = "Voice found"
-      this.voice = voices[0];
-
-    } else {
-      this.text = "netherlands voice not found"
+  info  = "loading"
+  textToSay = ""
+  voices: SpeechSynthesisVoice[] = []
+  languagesAllowed = ['en-GB', 'nl-NL', 'pl-PL', 'es-ES']
+  selectedLangIndex = 1;
+  voiceIndex = 0;
+  constructor() {
+    try {
+      this.voices = speechSynthesis.getVoices().filter(elem => elem.localService && this.languagesAllowed.includes(elem.lang))
+      if(this.voices.length > 0) {
+        this.info = ""
+        this.voiceIndex = this.voices.findIndex(elem => elem.lang == this.languagesAllowed[this.selectedLangIndex])
+      } else {
+        this.info = "Problem finding voices"
+      }
+    } catch(error) {
+      this.info = "There was a problem accesing the TTS api on your browser. try another browser :("
     }
+    
   }
 
-  sayStuff() {
-    console.log('saystuff clicked')
-    if(this.voice) {
-      const message = new SpeechSynthesisUtterance("Hallo, ik ben Alexander en de koning is echt heet");
-      message.voice = this.voice
-      message.lang = "nl-NL"
+
+  ngOnInit(): void {
+
+  }
+
+  async sayStuff() {
+    await speechSynthesis.cancel()
+      const message = new SpeechSynthesisUtterance(this.textToSay);
+      message.voice = this.voices[this.voiceIndex]
+      message.lang = this.languagesAllowed[this.selectedLangIndex]
       speechSynthesis.speak(message);
-    }
 
   }
 
