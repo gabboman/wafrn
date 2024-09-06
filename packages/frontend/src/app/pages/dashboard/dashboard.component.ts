e4df45b7ce5c998b0332ba33dd3a90e6888e6f67
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-09-07 00:51:39 +0200

    diable this part of ignoring the start date when closing post editor

--- packages/frontend/src/app/pages/dashboard/dashboard.component.ts
+++ packages/frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -9,7 +9,6 @@ import { ThemeService } from 'src/app/services/theme.service';
 import { MessageService } from 'src/app/services/message.service';
 import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
-import { EditorService } from 'src/app/services/editor.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -132,11 +131,13 @@ export class DashboardComponent implements OnInit, OnDestroy {
   async loadPosts(page: number) {
     this.loadingPosts = true;
     let scrollDate = new Date(this.timestamp);
+    /*
+    // diable this part of ignoring the start date when closing post editor
     if(page === 0 && EditorService.editorData) {
       scrollDate = EditorService.editorData.scrollDate;
-      console.log(scrollDate)
       EditorService.editorData = undefined;
     }
+    */
     if (page !== 0) {
       const lastPostBlock = this.posts[this.posts.length - 1];
       scrollDate = new Date(lastPostBlock[lastPostBlock.length - 1].createdAt);
