20d182efddcc8b517d38bc504ed3eda56e245ceb
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-09-13 15:33:48 +0200

    Fixed delivery issues

--- packages/backend/utils/queueProcessors/sendPostToInboxes.ts
+++ packages/backend/utils/queueProcessors/sendPostToInboxes.ts
@@ -3,17 +3,11 @@ import { logger } from '../logger'
 import { postPetitionSigned } from '../activitypub/postPetitionSigned'
 
 async function sendPostToInboxes(job: Job) {
-  const inboxes: string[] = job.data.inboxList
+  const inbox: string = job.data.inboxList
   const localUser = job.data.petitionBy
   const objectToSend = job.data.objectToSend
-  const promisesArray = inboxes.map((remoteInbox) => postPetitionSigned(objectToSend, localUser, remoteInbox))
-
-  try {
-    //at some point we should remove the array thing but at the same time yeah
-    const tmp = await Promise.all(promisesArray)
-  } catch (error) {
-    logger.debug(error)
-  }
+  //at some point we should remove the array thing but at the same time yeah
+  const tmp = await postPetitionSigned(objectToSend, localUser, inbox)
   return true
 }
 
