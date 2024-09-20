0488d3a1d9b62a563082e1b78ebc74b83a286ee0
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2024-09-20 19:36:57 +0200

    Updated email campaign

--- packages/backend/utils/maintenanceTasks/mailCampaing.ts
+++ packages/backend/utils/maintenanceTasks/mailCampaing.ts
@@ -18,19 +18,18 @@ async function sendMail() {
   })
 
   for await (const user of users) {
-    const subject = `Hello ${user.url}, we would like to ASK you about a new feature...`
+    const subject = `Hello ${user.url}, we have a new editor, new bugs!`
     const body = `
     <h1>What's up ${user.url}?</h1>
-<p>We have made a huge update to <a href="https://app.wafrn.net" target="_blank">wafrn</a>! Now you can do ASKS! You can enable anons too in your profile settings!</p>
 <a href="https://app.wafrn.net"><img style="width:100%" src="https://app.wafrn.net/assets/logo.png" /> </a>
-<p>New cool button!</p>
-<img style="width:100%" src="https://media.wafrn.net/1724617882050_ffb640d7272ffe73ba7eee8edbd121a5775781f6.avif" />
-<p>By default its only from logged in users. In the near future you will be able to recive asks from the fediverse with some dark magic on my side</p>
-<p >We have worked very hard! Come back, enable anons if thats your thing and send your profile to your friends!</p>
+<p>We have made a <del>huge</del> <del>gay</del> <del>very boring</del> medium sized update to <a href="https://app.wafrn.net" target="_blank">wafrn</a>! Now we have a new editor! It will not have the same bugs as the old one, but you can still force the old one in your profile settings :D</p>
+<img style="width:100%" src="https://cdn.wafrn.net/api/cache/?media=https%3A%2F%2Fmedia.wafrn.net%2F1726853570174_57660afcdf8571e15f5643ebc8466d5ab22224e6.avif" />
+<p>I would like to remind you that we also have asks :DDDDD</p>
 <img style="width:100%"  src="https://media.wafrn.net/1724617878850_a8ece1d9a501241bc7049022da9160c80d76538c.avif" />
 <p>Oh, almost forgot, as you can see we also have another minecraft server. Go join minecraft.wafrn.net (the ip has changed sorry), you can use bedrock or java. Do not worry, its compatible with old versions and modern ones! :D</p>
-<p>Also, if you have one spare buck, give it to me! I want to invite my wife to a pizza. We have <a href="https://patreon.com/wafrn">patreon</a> and <a href="https://ko-fi.com/wafrn">ko-fi</a>. You dont have to, but it would be cool</p>
+<p>And finaly, if you have one spare buck, give it to me! I want to invite my wife to a pizza. We have <a href="https://patreon.com/wafrn">patreon</a> and <a href="https://ko-fi.com/wafrn">ko-fi</a>. You dont have to, but it would be cool</p>
 <h2>Come back to <a href="https://app.wafrn.net">wafrn</a>, we miss you!</h2>
+<p>(Columbo voice): Oh, a just final thing, <a href="https://app.wafrn.net/fediverse/post/b574e933-0485-43ef-9a4e-c20f4d0a34ae">someone is working on an app</a></p>
     `
     console.log(`mailing ${user.url}`)
     await sendActivationEmail(user.email, '', subject, body)
