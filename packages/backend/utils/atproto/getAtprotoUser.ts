9e8a173a62f544c4361892eae326d554431d03d7
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2024-11-03 16:36:27 +0100

    Get bsky users working

---
+++ packages/backend/utils/atproto/getAtprotoUser.ts
@@ -0,0 +1,37 @@
+import {getAtProtoSession} from "./getAtProtoSession.js";
+import {User} from "../../db.js";
+
+
+async function getAtprotoUser(handle, localUser) {
+  const agent = await getAtProtoSession(localUser);
+  // TODO check if current user exist
+  const bskyUserResponse = await agent.getProfile({ actor: handle });
+  if(bskyUserResponse.success) {
+    const data = bskyUserResponse.data;
+    const newData = {
+      bskyDid: data.did,
+      url: '@' + data.handle,
+      name: data.displayName,
+      avatar: data.avatar,
+      description: data.description,
+      followingCount:  data.followsCount,
+      followersCount: data.followersCount,
+      headerImage: data.banner,
+    }
+    let userFound = await User.findOne({
+      where: {
+        bskyDid: data.did
+      }
+    });
+    console.log(newData)
+    if(userFound) {
+      await userFound.update(newData);
+      await userFound.save();
+    }else {
+      userFound = await User.create(newData)
+    }
+    return userFound
+  }
+}
+
+export {getAtprotoUser};
