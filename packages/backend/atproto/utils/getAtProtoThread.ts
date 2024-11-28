a32de9921016230159481f5a4bc6ddb99d9e82af
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2024-11-28 21:04:19 +0100

    ok now

--- packages/backend/atproto/utils/getAtProtoThread.ts
+++ packages/backend/atproto/utils/getAtProtoThread.ts
@@ -31,7 +31,7 @@ async function getAtProtoThread(uri: string, operation?: { operation: CreateOrUp
         record: operation.operation.record,
         cid: operation.operation.cid,
         uri: uri,
-        labels: operation.operation.labels,
+        labels: operation.operation.record.labels,
         indexedAt: new Date().toISOString(),
         author: {
           did: operation.remoteUser.bskyDid,
