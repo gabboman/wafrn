cb31eb5e59c370604fc3062dcbe5592c0055c648
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2024-12-18 19:53:26 +0100

    Clear the cache directory on every restart

--- packages/backend/remoteCache.ts
+++ packages/backend/remoteCache.ts
@@ -4,6 +4,10 @@ import { environment } from './environment.js'
 import { logger } from './utils/logger.js'
 import cacheRoutes from './routes/remoteCache.js'
 import checkIpBlocked from './utils/checkIpBlocked.js'
+import fs from 'fs'
+
+fs.rmSync('cache', { recursive: true, force: true });
+fs.mkdirSync('cache')
 
 const PORT = environment.cachePort
 
@@ -13,7 +17,6 @@ app.use(cors())
 app.set('trust proxy', 1)
 
 cacheRoutes(app)
-
 app.listen(PORT, environment.listenIp, () => {
   logger.info('Started fedi listener')
 })
