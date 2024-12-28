81b75b0e57517264be925315634477bbbe369d6a
FireIsGood <fireisadequate@gmail.com>
2024-12-27 21:59:35 -0800

    feat: prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,html}"],
+    excludedFiles: ["*inline-template-*.component.html"],
+    extends: [
+      "plugin:@angular-eslint/template/recommended",
+      "plugin:prettier/recommended",
+    ],
+    rules: {
+      "prettier/prettier": ["error", { parser: "angular" }],
+    },
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
