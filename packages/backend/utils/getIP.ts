db9c1524415b6632e201c063c89b2377ff4322ac
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-01-12 16:59:01 +0100

    hmm

--- packages/backend/utils/getIP.ts
+++ packages/backend/utils/getIP.ts
@@ -2,7 +2,7 @@ export default function getIp(petition: any): string {
   const res = petition.header('x-forwarded-for') || petition.connection.remoteAddress
   if (res.includes(',')) {
     // WHAT THE FUCK HOW DID YOU DO THIS
-    throw new Error('Invalid ip, ip has a comma')
+    throw new Error('Invalid ip, ip has a comma: ' + res)
   }
   return res
 }
