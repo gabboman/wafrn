69c5249b312215b059d9033c679dbd8a9ac49485
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-01-15 23:18:35 +0100

    Fixed posibility of post to be deleted to be undefined

--- packages/backend/atproto/workers/processFirehoseWorker.ts
+++ packages/backend/atproto/workers/processFirehoseWorker.ts
@@ -1,12 +1,12 @@
-import { Job } from "bullmq";
-import { getAtprotoUser } from "../utils/getAtprotoUser.js";
-import { Follows, Post, User, UserLikesPostRelations, PostTag, Media } from "../../db.js";
-import { environment } from "../../environment.js";
-import { Op, Model } from "sequelize";
-import { logger } from "../../utils/logger.js";
-import { DeleteOp, RepoOp } from "@skyware/firehose";
-import { getAtProtoThread } from "../utils/getAtProtoThread.js";
-import { getCacheAtDids } from "../cache/getCacheAtDids.js";
+import { Job } from 'bullmq'
+import { getAtprotoUser } from '../utils/getAtprotoUser.js'
+import { Follows, Post, User, UserLikesPostRelations, PostTag, Media } from '../../db.js'
+import { environment } from '../../environment.js'
+import { Op, Model } from 'sequelize'
+import { logger } from '../../utils/logger.js'
+import { DeleteOp, RepoOp } from '@skyware/firehose'
+import { getAtProtoThread } from '../utils/getAtProtoThread.js'
+import { getCacheAtDids } from '../cache/getCacheAtDids.js'
 import { deletePostCommon } from '../../utils/deletePost.js'
 import { redisCache } from '../../utils/redis.js'
 import { likePostRemote } from '../../utils/activitypub/likePost.js'
@@ -18,17 +18,17 @@ const adminUser = User.findOne({
 })
 async function processFirehose(job: Job) {
   // FIRST VERSION. THIS IS GONA BE DIRTY
-  const remoteUser = await getAtprotoUser(job.data.repo, await adminUser as Model<any, any>);
-  const operation: RepoOp = job.data.operation;
+  const remoteUser = await getAtprotoUser(job.data.repo, (await adminUser) as Model<any, any>)
+  const operation: RepoOp = job.data.operation
   if (remoteUser && operation) {
     switch (operation.action) {
       case 'create': {
-        const record = operation.record;
+        const record = operation.record
         switch (record['$type']) {
           case 'app.bsky.feed.like': {
             if ((await getCacheAtDids()).followedDids.includes(job.data.repo)) {
-              const postLikedUri = record.subject.uri;
-              const postId = await getAtProtoThread(postLikedUri);
+              const postLikedUri = record.subject.uri
+              const postId = await getAtProtoThread(postLikedUri)
               if (postId) {
                 await UserLikesPostRelations.findOrCreate({
                   where: {
@@ -43,7 +43,7 @@ async function processFirehose(job: Job) {
                 where: {
                   bskyUri: record.subject.uri
                 }
-              });
+              })
               if (postInDb) {
                 await UserLikesPostRelations.findOrCreate({
                   where: {
@@ -54,15 +54,15 @@ async function processFirehose(job: Job) {
                 })
               }
             }
-            break;
+            break
           }
           case 'app.bsky.feed.post': {
-            const postBskyUri = `at://${job.data.repo}/${operation.path}`;
+            const postBskyUri = `at://${job.data.repo}/${operation.path}`
             await getAtProtoThread(postBskyUri, { operation, remoteUser })
-            break;
+            break
           }
           case 'app.bsky.feed.repost': {
-            const postToBeRewooted = await getAtProtoThread(record.subject.uri);
+            const postToBeRewooted = await getAtProtoThread(record.subject.uri)
             if (postToBeRewooted) {
               const rewootCreated = await Post.create({
                 content: '',
@@ -74,31 +74,31 @@ async function processFirehose(job: Job) {
               })
             }
 
-            break;
+            break
           }
           case 'app.bsky.graph.follow': {
-            const userFollowed = await getAtprotoUser(record.subject, await adminUser as Model<any, any>);
+            const userFollowed = await getAtprotoUser(record.subject, (await adminUser) as Model<any, any>)
             if (userFollowed) {
               let tmp = await Follows.create({
                 followedId: userFollowed.id,
                 followerId: remoteUser.id,
                 bskyPath: operation.path,
-                accepted: true,
-              });
+                accepted: true
+              })
             }
-            break;
+            break
           }
           default: {
             logger.warn({ message: `Bsky create type not implemented: ${record['$type']}`, record: record })
           }
         }
-        break;
+        break
       }
       case 'delete': {
         // you need to check the path and do a deleete based on that.
-        const deleteOperation = operation as DeleteOp;
+        const deleteOperation = operation as DeleteOp
         try {
-          const opName = deleteOperation.path.split('app.bsky.')[1].split('/')[0];
+          const opName = deleteOperation.path.split('app.bsky.')[1].split('/')[0]
           switch (opName) {
             case 'graph.follow': {
               await Follows.destroy({
@@ -106,66 +106,61 @@ async function processFirehose(job: Job) {
                   bskyPath: operation.path
                 }
               })
-              break;
+              break
             }
 
             case 'feed.post': {
               const post = await Post.findOne({
                 where: {
-                  bskyUri: {
-                    [Op.like]: `%${operation.path}`
-                  }
+                  bskyUri: `at://${job.data.repo}/${operation.path}`
                 }
-              });
-              await redisCache.del('postAndUser:' + post.id)
-              await deletePostCommon(post.id)
-
-              const id = post.id as string
-              if (id) {
-                let postsToDeleteUnfiltered = await Post.findAll({
-                  where: {
-                    parentId: id,
-                    content: ''
-                  }
-                })
-
-                const unfilteredPostIds: string[] = postsToDeleteUnfiltered.map((elem: any) => elem.id)
-                const tags = await PostTag.findAll({
-                  where: {
-                    postId: {
-                      [Op.in]: unfilteredPostIds
-                    }
-                  }
-                })
-
-                const medias = await Media.findAll({
-                  where: {
-                    postId: {
-                      [Op.in]: unfilteredPostIds
-                    }
-                  }
-                })
-
-                const postsThatAreNotReblogs = tags
-                  .map((tag: any) => tag.postId)
-                  .concat(medias.map((media: any) => media.postId))
-                const reblogsToDelete = unfilteredPostIds.filter((elem) => !postsThatAreNotReblogs.includes(elem))
-
-                if (reblogsToDelete) {
-                  reblogsToDelete.forEach(async (elem) => {
-                    await redisCache.del('postAndUser:' + elem)
-                  })
-
-                  await Post.destroy({
+              })
+              if (post) {
+                await redisCache.del('postAndUser:' + post.id)
+                await deletePostCommon(post.id)
+                const id = post.id
+                if (id) {
+                  let postsToDeleteUnfiltered = await Post.findAll({
                     where: {
-                      id: {
-                        [Op.in]: reblogsToDelete
+                      parentId: id,
+                      content: ''
+                    }
+                  })
+                  const unfilteredPostIds: string[] = postsToDeleteUnfiltered.map((elem: any) => elem.id)
+                  const tags = await PostTag.findAll({
+                    where: {
+                      postId: {
+                        [Op.in]: unfilteredPostIds
                       }
                     }
                   })
+                  const medias = await Media.findAll({
+                    where: {
+                      postId: {
+                        [Op.in]: unfilteredPostIds
+                      }
+                    }
+                  })
+                  const postsThatAreNotReblogs = tags
+                    .map((tag: any) => tag.postId)
+                    .concat(medias.map((media: any) => media.postId))
+                  const reblogsToDelete = unfilteredPostIds.filter((elem) => !postsThatAreNotReblogs.includes(elem))
+                  if (reblogsToDelete) {
+                    reblogsToDelete.forEach(async (elem) => {
+                      await redisCache.del('postAndUser:' + elem)
+                    })
+                    await Post.destroy({
+                      where: {
+                        id: {
+                          [Op.in]: reblogsToDelete
+                        }
+                      }
+                    })
+                  }
                 }
               }
-              break;
+
+              break
             }
 
             case 'feed.like': {
@@ -179,7 +174,7 @@ async function processFirehose(job: Job) {
                 likePostRemote(like, true)
                 await like.destroy()
               }
-              break;
+              break
             }
 
             default: {
@@ -193,17 +188,15 @@ async function processFirehose(job: Job) {
             error
           })
         }
-        break;
+        break
       }
       default: {
         logger.warn({ message: `Bsky action not implemented: ${operation.action}`, operation: operation })
       }
     }
-
   } else {
     logger.debug(`Failed to find remote bsky`)
   }
-
 }
 
 export { processFirehose }
