f72d3ed8715a45dbaa02f3298a987882cf7c04f2
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-01-18 14:48:47 +0100

    Revert "Improved stuff"
    This reverts commit 27d63bc8181ea7305b20cdc528e0d15d2d76ca22.

--- packages/backend/atproto/utils/checkCommitMentions.ts
+++ packages/backend/atproto/utils/checkCommitMentions.ts
@@ -1,57 +1,39 @@
-import { ParsedCommit } from '@skyware/firehose'
-import { Post } from '../../db.js'
-import { Op, Sequelize } from 'sequelize'
-import { getLocalUserId } from '../../utils/cacheGetters/getLocalUserId.js'
-import { getAllLocalUserIds } from '../../utils/cacheGetters/getAllLocalUserIds.js'
+import { ParsedCommit } from "@skyware/firehose";
+import { Post } from "../../db.js";
+import { Op, Sequelize } from "sequelize";
+import { getLocalUserId } from "../../utils/cacheGetters/getLocalUserId.js";
+import { getAllLocalUserIds } from "../../utils/cacheGetters/getAllLocalUserIds.js";
 
 // Preemptive checks to see if
-async function checkCommitMentions(
-  commit: ParsedCommit,
-  cacheData: { followedDids: string[]; localUserDids: string[]; followedUsersLocalIds: string[] }
-): Promise<boolean> {
-  const didsToCheck = [...new Set(cacheData.localUserDids.concat(cacheData.followedDids))]
+async function checkCommitMentions(commit: ParsedCommit, cacheData: { followedDids: string[], localUserDids: string[], followedUsersLocalIds: string[] }): Promise<boolean> {
+  const didsToCheck = [... new Set(cacheData.localUserDids.concat(cacheData.followedDids))];
   const followedUsersLocalIds = cacheData.followedUsersLocalIds
 
-  let res = false
+  let res = false;
   // first we check if there are any mentions to local users. if so we return true
   for (const operation of commit.ops) {
     // we check lik
-    if (
-      operation.action === 'create' &&
-      (operation.path.startsWith('app.bsky.feed.like') ||
-        operation.path.startsWith('app.bsky.graph.follow') ||
-        operation.path.startsWith('app.bsky.feed.post'))
-    ) {
+    if (operation.action === 'create' && operation.path.startsWith('app.bsky.feed.like')) {
       // we do not ned 18k likes on a mark hamill post. We better do just a "people you follow liked..."
-      if (didsToCheck.some((elem) => elem == commit.repo)) {
-        return true
+      if (didsToCheck.some(elem => elem == commit.repo)) {
+        return true;
       }
     }
     if (operation.action === 'create' && operation.path.startsWith('app.bsky.feed.post') && operation.record.facets) {
-      const mentions = operation.record.facets
-        .flatMap((elem) => elem.features)
-        .map((elem) => elem.did)
-        .filter((elem) => elem)
-      if (mentions && mentions.length && mentions.some((mention) => didsToCheck.includes(mention))) {
-        res = true
-        return res
+      const mentions = operation.record.facets.flatMap(elem => elem.features).map(elem => elem.did).filter(elem => elem)
+      if (mentions && mentions.length && mentions.some(mention => didsToCheck.includes(mention))) {
+        res = true;
+        return res;
       }
     }
   }
   // second one first approach: is post being replied on db? if so we store it.
-  const urisToCheck: string[] = commit.ops
-    .filter((op) => op.action === 'create' && op.path.startsWith('app.bsky.feed.post') && op.record?.reply)
-    .map((op) => {
-      return { parent: op.record.reply.parent.uri, root: op.record.reply.root.uri }
-    })
-    .map((elem) => [elem.parent, elem.root])
-    .flat()
-    .map((elem) => elem.split('at://')[1])
-    .map((elem) => elem.split('/app.bsky.feed')[0])
-  let postsFounds = 0
+  const urisToCheck: string[] = commit.ops.filter(op => op.action === 'create' && op.path.startsWith('app.bsky.feed.post') && op.record?.reply).map(op => { return { parent: op.record.reply.parent.uri, root: op.record.reply.root.uri } }).map(elem => [elem.parent, elem.root]).flat().map(elem => elem.split('at://')[1]).map(elem => elem.split('/app.bsky.feed')[0])
+  let postsFounds = 0;
+
 
   if (urisToCheck.length > 0) {
-    postsFounds = urisToCheck.some((elem) => didsToCheck.includes(elem)) ? 1 : 0
+    postsFounds = urisToCheck.some(elem => didsToCheck.includes(elem)) ? 1 : 0
     /*
     // if post starts with uri of any of our users it must be in reply to one of our users!
     if (urisToCheck.map(elem => elem.split('/app.bsky.feed.post/')[0]).some(elem => didsToCheck.includes(elem))) {
@@ -78,9 +60,10 @@ async function checkCommitMentions(
   }
 
   if (postsFounds > 0) {
-    res = true
+    res = true;
   }
-  return res
+  return res;
 }
+
 
 export { checkCommitMentions }
