b42158c0ac9d1bc53936232ad7902576816757e6
Juan D. Jara <juanorigami@gmail.com>
2025-02-23 00:00:23 +0100

    fix typo

--- packages/backend/utils/cacheGetters/getMutedPosts.ts
+++ packages/backend/utils/cacheGetters/getMutedPosts.ts
@@ -4,7 +4,7 @@ import { redisCache } from '../redis.js'
 
 async function getMutedPosts(userId: string, superMute = false): Promise<Array<string>> {
   let res: string[] = []
-  const cacheResult = await redisCache.get((superMute ? 'superMutedPosts' : 'mutedPosts:') + userId)
+  const cacheResult = await redisCache.get((superMute ? 'superMutedPosts:' : 'mutedPosts:') + userId)
   if (cacheResult) {
     res = JSON.parse(cacheResult)
   } else {
