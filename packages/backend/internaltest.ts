ee7e7be386c12ce2b1c08b1744f260855e411af4
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-03-09 16:05:45 +0100

    example post changed

--- packages/backend/internaltest.ts
+++ packages/backend/internaltest.ts
@@ -1,3 +1,4 @@
 import { getAtProtoThread } from './atproto/utils/getAtProtoThread.js'
 
-await getAtProtoThread('at://did:plc:zmgp4bhcck7kdxs5og7qo5rm/app.bsky.feed.post/3lfskv66r4s2d', undefined, true)
+// https://bsky.app/profile/did:plc:daplto33uqatqvce5nrjdwrz/post/3ljtctattrc2i
+await getAtProtoThread('at://did:plc:daplto33uqatqvce5nrjdwrz/app.bsky.feed.post/3ljtctattrc2i', undefined, true)
