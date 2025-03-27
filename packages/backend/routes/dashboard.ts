6ded814d57c1b43971cf4fa66a8ada1baffe27da
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-03-27 18:21:54 +0100

    Undone change

--- packages/backend/routes/dashboard.ts
+++ packages/backend/routes/dashboard.ts
@@ -142,21 +142,6 @@ export default function dashboardRoutes(app: Application) {
         attributes: ['id'],
         where: {
           createdAt: { [Op.lt]: getStartScrollParam(req) },
-          '$user.banned$': {
-            [Op.ne]: true
-          },
-          [Op.or]: [
-            {
-              '$user.federatedHostId$': {
-                [Op.ne]: null
-              }
-            },
-            {
-              '$user.federatedHost.blocked$': {
-                [Op.ne]: true
-              }
-            }
-          ],
           ...whereObject
         }
       })
