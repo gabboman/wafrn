6b11ba47f5bbfb917d0aeba8850ed08d2ad1a678
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-04-17 13:31:56 +0200

    Optimized get replies because could use some cache

--- packages/backend/utils/activitypub/getPostReplies.ts
+++ packages/backend/utils/activitypub/getPostReplies.ts
@@ -1,24 +1,40 @@
 import { Op } from 'sequelize'
-import { Post } from '../../db.js'
+import { Post, User } from '../../db.js'
 import { environment } from '../../environment.js'
+import { redisCache } from '../redis.js'
 
 async function getPostReplies(postId: string) {
-  // TODO: cache this. Also make it so if the post has no text but medias or tags also apears on the list
-  const posts = await Post.findAll({
-    attributes: ['id', 'parentId', 'remotePostId', 'privacy'],
-    where: {
-      content: {
-        [Op.ne]: ''
-      },
-      parentId: postId,
-      privacy: {
-        [Op.notIn]: [2, 10]
+  let resString = await redisCache.get('postReplies:' + postId)
+  if (!resString) {
+    const posts = await Post.findAll({
+      attributes: ['id', 'parentId', 'remotePostId', 'privacy'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          required: true,
+          where: {
+            banned: false
+          }
+        }
+      ],
+      where: {
+        isReblog: false,
+        parentId: postId,
+        privacy: {
+          [Op.notIn]: [2, 10]
+        }
       }
-    }
-  })
-  return posts.map((elem: any) =>
-    elem.remotePostId ? elem.remotePostId : `${environment.frontendUrl}/fediverse/post/${elem.id}`
-  )
+    })
+    resString = JSON.stringify(
+      posts.map((elem: any) =>
+        elem.remotePostId ? elem.remotePostId : `${environment.frontendUrl}/fediverse/post/${elem.id}`
+      )
+    )
+    await redisCache.set('postReplies:' + postId, resString, 'EX', 60)
+  }
+
+  return JSON.parse(resString)
 }
 
 export { getPostReplies }
