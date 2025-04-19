f11174191c5b8b5a7a9729d890f60be5f6181eb7
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-04-19 20:16:59 +0200

    hmm

--- packages/backend/updateDatabase/regenerateKeys.ts
+++ packages/backend/updateDatabase/regenerateKeys.ts
@@ -3,32 +3,27 @@ import { User, sequelize } from '../db.js'
 import { Op } from 'sequelize'
 import { generateKeyPairSync } from 'crypto'
 
-const usersToUpdate = User.findAll({
+const usersToUpdate = await User.findAll({
   where: {
     email: { [Op.ne]: null },
     privateKey: { [Op.eq]: null }
   }
 })
 
-usersToUpdate.then((users: any[]) => {
-  users.forEach((user: any) => {
-    const { publicKey, privateKey } = generateKeyPairSync('rsa', {
-      modulusLength: 4096,
-      publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
-      },
-      privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem'
-      }
-    })
-    user.publicKey = publicKey
-    user.privateKey = privateKey
-    console.log(`updating ${user.url}`)
-
-    user.save().then(() => {
-      console.log(`Updated ${user.url}`)
-    })
+for await (const user of usersToUpdate) {
+  const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem'
+    }
   })
-})
+  user.publicKey = publicKey
+  user.privateKey = privateKey
+  console.log(`updating ${user.url}`)
+  await user.save()
+}
