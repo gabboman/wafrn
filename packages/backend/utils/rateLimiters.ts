f4da1e8e320ddf8d2bdceb5c86fa0072ed63b6c2
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-04-25 00:03:50 +0200

    updated ratelimit to more than one petition each 2 seconds. that may be too much

--- packages/backend/utils/rateLimiters.ts
+++ packages/backend/utils/rateLimiters.ts
@@ -31,7 +31,7 @@ const loginRateLimiter = rateLimit({
 
 const navigationRateLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 30, // Limit each IP to 30 requests per `window` (here, per minute)
+  max: 40, // Limit each IP to 40 requests per `window` (here, per minute)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: 'Too many navigation requests'
