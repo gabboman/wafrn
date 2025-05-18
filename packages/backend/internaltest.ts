314e18ac4ae853ef63cbc6a3173d95fa93544f03
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-05-18 22:54:12 +0200

    test for verbina post

--- packages/backend/internaltest.ts
+++ packages/backend/internaltest.ts
@@ -2,41 +2,45 @@
 
 import { Op } from 'sequelize'
 import { getAtProtoThread } from './atproto/utils/getAtProtoThread.js'
-import { User } from './models/index.js'
+import { Media, Post, PostTag, Quotes, User } from './models/index.js'
 import { environment } from './environment.js'
 import { getRemoteActor } from './utils/activitypub/getRemoteActor.js'
 import { MoveActivity } from './utils/activitypub/processors/move.js'
 import sendActivationEmail from './utils/sendActivationEmail.js'
 import { wait } from './utils/wait.js'
+import { getAtProtoSession } from './atproto/utils/getAtProtoSession.js'
+import { postToAtproto } from './atproto/utils/postToAtproto.js'
 
 // https://bsky.app/profile/did:plc:kcu5gsklhhensnm6vhu6lhq5/post/3lkw3tgtihs23
 //await getAtProtoThread('at://did:plc:kcu5gsklhhensnm6vhu6lhq5/app.bsky.feed.post/3lljrwzmx522w', undefined, true)
 
-async function sendMail() {
-  const users = await User.findAll({
-    attributes: ['url', 'email', 'banned', 'activated', 'disableEmailNotifications'],
+const post = (await Post.findByPk('72d87a3f-0bc7-4c53-988c-c9e29e2ae6b7')) as Post
+const user = (await User.findByPk(post.userId)) as User
+const agent = await getAtProtoSession(user)
+let isReblog = false
+if (post.content == '' && post.content_warning == '' && post.parentId) {
+  const mediaCount = await Media.count({
     where: {
-      banned: { [Op.ne]: true },
-      activated: true,
-      disableEmailNotifications: false,
-      email: {
-        [Op.ne]: null
-      },
-      createdAt: {
-        [Op.gt]: new Date().setDate(new Date().getDate() - 2)
-      }
-    },
-    order: [['createdAt', 'ASC']]
+      postId: post.id
+    }
   })
-  console.log('NUMBER OF EMAILS TO SEND: ' + users.length)
-  await wait(1500)
-  for await (const user of users) {
-    const subject = `Hello ${user.url}! Your wafrn account was activated!`
-    const body = `Hello ${user.url}, your account has been reviewed by our team and is now activated! If you already got this email twice apologies, we got an issue with the email provider and we had to resend a few :(`
-    console.log(`mailing ${user.url}`)
-    await sendActivationEmail(user.email || '', '', subject, body)
-    await wait(1500)
+  const quotesCount = await Quotes.count({
+    where: {
+      quoterPostId: post.id
+    }
+  })
+  const tagsCount = await PostTag.count({
+    where: {
+      postId: post.id
+    }
+  })
+  if (mediaCount + quotesCount + tagsCount === 0) {
+    isReblog = true
   }
 }
-
-sendMail()
+if (!isReblog) {
+  const bskyPost = await agent.post(await postToAtproto(post, agent))
+  post.bskyUri = bskyPost.uri
+  post.bskyCid = bskyPost.cid
+  await post.save()
+}
