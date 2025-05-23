bededc45b7c84d3bbe2d855b764210a7e4136cbb
Zsolt Sz. Sz. Raven <mail@sztupy.hu>
2025-05-23 22:48:27 +0000

    Add support for backup cache providers
    Since the entire wafrn stack is usually running on a single box, it has a single IP. When requesting resources from external sources it can hit IP limits easily.
    
    This change will allow the bluesky/ATproto media cache function to use external backups. If the main request fails for any reason it will try one of the backup URLs as a fallback, and see if it can get the data through it.
    
    An example implementation of the bacup provider is now built and deploted to Vercel. To configure it either natively:
    
    ```javascript
      externalCacheBackups: ['https://did-decoder-lambda.vercel.app/api/cache?media='],
    ```
    
    or through the `.env` file (Note: the docker-compose.yml files have changed so they need to be updated to support this new env var):
    
    ```sh
    FRONTEND_CACHE_BACKUP_URLS="'https://did-decoder-lambda.vercel.app/api/cache?media='"
    ```
    
    The source code of the backup DID decoder is available at https://github.com/sztupy/did-decoder-lambda

--- packages/backend/environment.local.example.ts
+++ packages/backend/environment.local.example.ts
@@ -30,6 +30,9 @@ export const environment = {
   mediaUrl: 'https://localhost/api/uploads',
   // You should run also this project github.com/gabboman/fediversemediacacher. In my case, https://cache.wafrn.net/?media= The cache is there because at some point in the past I configured it to precache images. No need for it to be honest
   externalCacheurl: 'https://localhost/api/cache?media=',
+  // If main cache fails due to IP limits you can install additional proxies, and use them here. The cache will try these as well before failing.
+  // You can deploy https://github.com/sztupy/did-decoder-lambda this project to Netlify or Vercel as a backup for example
+  externalCacheBackups: [],
   // after the first run, create the admin user. and a deleted user. You will have to edit the user url in db so it starts with an @
   adminUser: 'admin',
   // admin email wich you will recive things like "someone registred and you need to review this"
