5b4c0cbf4820415e14648a3088666560f66cc38a
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-05-25 11:40:09 +0200

    force update of followed bsky users because avatars

--- packages/backend/internaltest.ts
+++ packages/backend/internaltest.ts
@@ -10,37 +10,31 @@ import sendActivationEmail from './utils/sendActivationEmail.js'
 import { wait } from './utils/wait.js'
 import { getAtProtoSession } from './atproto/utils/getAtProtoSession.js'
 import { postToAtproto } from './atproto/utils/postToAtproto.js'
+import { LdSignature } from './utils/activitypub/rsa2017.js'
+import { activityPubObject } from './interfaces/fediverse/activityPubObject.js'
+import { Queue } from 'bullmq'
+import { getCacheAtDids } from './atproto/cache/getCacheAtDids.js'
+import { getAtprotoUser } from './atproto/utils/getAtprotoUser.js'
 
-// https://bsky.app/profile/did:plc:kcu5gsklhhensnm6vhu6lhq5/post/3lkw3tgtihs23
-//await getAtProtoThread('at://did:plc:kcu5gsklhhensnm6vhu6lhq5/app.bsky.feed.post/3lljrwzmx522w', undefined, true)
+const cacheDids = await getCacheAtDids(true)
 
-const post = (await Post.findByPk('72d87a3f-0bc7-4c53-988c-c9e29e2ae6b7')) as Post
-const user = (await User.findByPk(post.userId)) as User
-const agent = await getAtProtoSession(user)
-let isReblog = false
-if (post.content == '' && post.content_warning == '' && post.parentId) {
-  const mediaCount = await Media.count({
-    where: {
-      postId: post.id
+const followedDids = cacheDids.followedUsersLocalIds
+
+const users = await User.findAll({
+  where: {
+    bskyDid: {
+      [Op.in]: Array.from(followedDids)
     }
-  })
-  const quotesCount = await Quotes.count({
-    where: {
-      quoterPostId: post.id
-    }
-  })
-  const tagsCount = await PostTag.count({
-    where: {
-      postId: post.id
-    }
-  })
-  if (mediaCount + quotesCount + tagsCount === 0) {
-    isReblog = true
   }
-}
-if (!isReblog) {
-  const bskyPost = await agent.post(await postToAtproto(post, agent))
-  post.bskyUri = bskyPost.uri
-  post.bskyCid = bskyPost.cid
-  await post.save()
+})
+
+const adminuser = (await User.findOne({
+  where: {
+    url: environment.adminUser
+  }
+})) as User
+
+for await (const user of users) {
+  console.log(`Updating ${user.url}`)
+  await getAtprotoUser(user.url, adminuser)
 }
