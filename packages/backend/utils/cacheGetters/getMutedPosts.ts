a5dd0d31107591d96721e5697ded838cacbe2d4a
Juan D. Jara <juanorigami@gmail.com>
2025-06-12 12:13:09 +0200

    this is how we should have fixed this

--- packages/backend/utils/cacheGetters/getMutedPosts.ts
+++ packages/backend/utils/cacheGetters/getMutedPosts.ts
@@ -34,9 +34,6 @@ async function getMutedPosts(userId: string, superMute = false): Promise<Array<s
 }
 
 async function getMutedPostsMultiple(userIds: string[], superMute = false) {
-  if(userIds.length == 0) {
-    return []
-  }
   let cacheResults: (string | null) [] = []
   try {
     cacheResults = await redisCache.mget(
@@ -50,7 +47,6 @@ async function getMutedPostsMultiple(userIds: string[], superMute = false) {
       error
     })
   }
-  
 
   if (cacheResults.every((result) => !!result)) {
     const ids = cacheResults.map((result) => JSON.parse(result!) as string[])
