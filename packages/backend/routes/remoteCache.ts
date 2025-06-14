d1e5667ce4c1194091f07a96820f61a6f1c21f05
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-06-14 18:29:37 +0200

    simplify cacher

--- packages/backend/routes/remoteCache.ts
+++ packages/backend/routes/remoteCache.ts
@@ -39,122 +39,53 @@ function writeStream(stream: AxiosResponse, localFileName: string) {
 
 export default function cacheRoutes(app: Application) {
   app.get('/api/cache', async (req: Request, res: Response) => {
-    const mediaUrl = String(req.query?.media)
+    let mediaUrl = String(req.query?.media)
+    const mediaLinkHash = crypto.createHash('sha256').update(mediaUrl).digest('hex')
+    let localFileName = `cache/${mediaLinkHash}`
     const avatarTransform = String(req.query?.avatar) === 'true'
 
     if (!mediaUrl) {
       res.sendStatus(404)
       return
     }
-
-    try {
-      // TODO: to support bluesky images, we should receive full URLs built on frontend and not just cids
-      if (mediaUrl.startsWith('?cid=')) {
-        let fileName
-        try {
-          const did = decodeURIComponent(mediaUrl.split('&did=')[1])
-          const cid = decodeURIComponent(mediaUrl.split('&did=')[0].split('?cid=')[1])
-          if (!did || !cid) {
-            return res.sendStatus(400)
-          }
-
-          fileName = `cache/bsky_${cid}`
-          if (fs.existsSync(fileName)) {
-            return sendWithCache(res, fileName)
-          }
-
-          const plcResolver = getResolver()
-          const didResolver = new Resolver(plcResolver)
-          const didData = await didResolver.resolve(did)
-          if (didData?.didDocument?.service) {
-            const url =
-              didData.didDocument.service[0].serviceEndpoint +
-              '/xrpc/com.atproto.sync.getBlob?did=' +
-              encodeURIComponent(did) +
-              '&cid=' +
-              encodeURIComponent(cid)
-
-            const remoteResponse = await axios.get(url, {
-              responseType: 'stream',
-              headers: { 'User-Agent': environment.instanceUrl }
-            })
-
-            await writeStream(remoteResponse, fileName)
-            return sendWithCache(res, fileName)
-          }
-        } catch (error) {
-          if (fileName && environment.externalCacheBackups?.length > 0) {
-            const backupServer =
-              environment.externalCacheBackups[Math.floor(Math.random() * environment.externalCacheBackups.length)]
-
-            try {
-              const remoteResponse = await axios.get(backupServer + encodeURIComponent(mediaUrl), {
-                responseType: 'stream',
-                headers: { 'User-Agent': environment.instanceUrl }
-              })
-
-              await writeStream(remoteResponse, fileName)
-              return sendWithCache(res, fileName)
-            } catch (e2) {
-              error = e2
+    // if file exists
+    if (fs.existsSync(localFileName)) {
+      return await sendWithCache(res, localFileName)
+    } else {
+      try {
+        if (mediaUrl.startsWith('?cid=')) {
+          try {
+            const did = decodeURIComponent(mediaUrl.split('&did=')[1])
+            const cid = decodeURIComponent(mediaUrl.split('&did=')[0].split('?cid=')[1])
+            if (!did || !cid) {
+              return res.sendStatus(400)
             }
+            const plcResolver = getResolver()
+            const didResolver = new Resolver(plcResolver)
+            const didData = await didResolver.resolve(did)
+            if (didData?.didDocument?.service) {
+              const url =
+                didData.didDocument.service[0].serviceEndpoint +
+                '/xrpc/com.atproto.sync.getBlob?did=' +
+                encodeURIComponent(did) +
+                '&cid=' +
+                encodeURIComponent(cid)
+              mediaUrl = url
+            }
+          } catch (error) {
+            return res.sendStatus(500)
           }
-
-          logger.trace({
-            message: 'error on cache with dids',
-            url: req.query?.media,
-            error: error
-          })
-          return res.sendStatus(500)
         }
-      }
-
-      // this urlBase will always reflect the starting URL requested from the client, the one that is in the browser, not the internal one behind the proxies
-      const urlBase = req.protocol + '://' + req.get('host') + req.originalUrl
-      // with the second parameter to the URL constructor we can provide a base URL in case the media URL is something like "/api/uploads/..." so the URL constructor does not throw an error
-      const url = new URL(mediaUrl, urlBase)
-      const mediaLink = url.href as string
-
-      const mediaLinkHash = crypto.createHash('sha256').update(mediaLink).digest('hex')
-      let localFileName = await redisCache.get(`cache:${mediaLinkHash}`)
-
-      if (!localFileName || !fs.existsSync(localFileName)) {
-        const stream = await axios.get(mediaLink, {
+        const response = await axios.get(mediaUrl, {
           responseType: 'stream',
-          headers: { 'User-Agent': environment.instanceUrl }
+          headers: { 'User-Agent': 'wafrnCacher' }
         })
-        // read mime type (for the extension) from the headers of the remote response
-        const mimeType = String(stream.headers['Content-Type'] || stream.headers['content-type'] || '')
-        const ext = extensionFromMimeType(mimeType)
-        localFileName = `cache/${mediaLinkHash}.${ext}`
 
-        await redisCache.set(`cache:${mediaLinkHash}`, localFileName)
-        await writeStream(stream, localFileName)
+        response.data.pipe(res)
+        await writeStream(response, localFileName)
+      } catch (error) {
+        return res.sendStatus(500)
       }
-      // at this point we have the file in the local file system and the hash-filename relation in the redis cache
-
-      // if the avatarTransform flag is true, we need to optimize the file and save the optimized copy in the local file system
-      if (avatarTransform) {
-        const avatarFileName = `cache/avatars_${mediaLinkHash}.webp`
-        if (fs.existsSync(avatarFileName)) {
-          localFileName = avatarFileName
-        } else {
-          localFileName = await optimizeMedia(localFileName, {
-            outPath: avatarFileName,
-            maxSize: 96,
-            keep: true
-          })
-        }
-      }
-
-      return sendWithCache(res, localFileName)
-    } catch (error) {
-      logger.trace({
-        message: 'error on cache',
-        url: mediaUrl,
-        error: error
-      })
-      return res.sendStatus(500)
     }
   })
 
