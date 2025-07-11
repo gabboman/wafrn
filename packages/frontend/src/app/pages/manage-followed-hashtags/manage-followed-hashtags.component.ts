53ad7864cb53201e440fc9a1fdcda9d75d74d9c7
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-07-11 23:01:52 +0100

    Also follow new tags from this page

--- packages/frontend/src/app/pages/manage-followed-hashtags/manage-followed-hashtags.component.ts
+++ packages/frontend/src/app/pages/manage-followed-hashtags/manage-followed-hashtags.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { FormsModule, ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 import { RouterModule } from '@angular/router'
 import { LoaderComponent } from 'src/app/components/loader/loader.component'
 import { DashboardService } from 'src/app/services/dashboard.service'
@@ -10,12 +13,23 @@ import { PostsService } from 'src/app/services/posts.service'
 
 @Component({
   selector: 'app-manage-followed-hashtags',
-  imports: [CommonModule, MatCardModule, RouterModule, MatButtonModule, LoaderComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    RouterModule,
+    MatButtonModule,
+    LoaderComponent,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './manage-followed-hashtags.component.html',
   styleUrl: './manage-followed-hashtags.component.scss'
 })
 export class ManageFollowedHashtagsComponent {
   loading = true
+  tag = ''
   constructor(
     public postsService: PostsService,
     private dashboardService: DashboardService,
@@ -27,9 +41,12 @@ export class ManageFollowedHashtagsComponent {
     })
   }
 
-  async unfollowHashtag(tag: string) {
+  async updateHashtag(tag: string, follow: boolean) {
     this.loading = true
-    const success = await this.dashboardService.manageHashtagSubscription(tag, false)
+    if (follow) {
+      this.tag = ''
+    }
+    const success = await this.dashboardService.manageHashtagSubscription(tag, follow)
     this.messageService.add({
       severity: success ? 'success' : 'error',
       summary: success ? `You no longer follow #${tag}` : 'Something went wrong!'
