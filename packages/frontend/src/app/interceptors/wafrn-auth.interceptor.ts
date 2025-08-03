a2e3e9fd1c39e571918ded9556f48dd3c8539b69
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-08-03 18:29:38 +0200

    oh it may be this

--- packages/frontend/src/app/interceptors/wafrn-auth.interceptor.ts
+++ packages/frontend/src/app/interceptors/wafrn-auth.interceptor.ts
@@ -12,18 +12,14 @@ export class WafrnAuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let authReq = req
     const token = localStorage.getItem('authToken')
-    if (token != null && req.url.indexOf(EnvironmentService.environment.baseUrl) !== -1) {
+    if (
+      token != null &&
+      (req.url.indexOf(EnvironmentService.environment.baseUrl) !== -1 || req.url.startsWith('/api'))
+    ) {
       authReq = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) })
-    } else {
-      console.log({
-        baseUrl: EnvironmentService.environment.baseUrl,
-        petitionUr: req.url
-      })
     }
     return next.handle(authReq).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(authReq.headers)
-        console.log('ERROR UNAUTHORIZED. Token: ' + token)
         if (error.status === 401) {
           localStorage.clear()
           this.router.navigate(['/register'])
